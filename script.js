bcf7edf3a8b9cf707360eb08782b0cc0701a4d5b
Antoine <tadgithub@gmail.com>
2022-04-07 18:02:08 -0400

    Added search function for API

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const animeApp = {};
 
-animeApp.init = ()=>{
+animeApp.sendInput = ()=>{
+    document.querySelector("form").addEventListener( "submit", e => {
+        e.preventDefault();
+        const userInput = document.querySelector("input");
+        const inputValue = userInput.value;
+        animeApp.getAnimeData(inputValue);
+    })
+}
 
+animeApp.getAnimeData = (userInput) => {
+    const url = new URL(`https://api.jikan.moe/v4/anime`)
+    url.search = new URLSearchParams({
+        q: userInput.toString(),
+        limit: 4,
+    })
+    fetch(url)
+    .then((res) => res.json())
+    .then((animeData) => {
+        // console.log(animeData);
+    })
+}
+
+animeApp.init = ()=>{
+    animeApp.sendInput();
+    // animeApp.getInput();
 };
 
+animeApp.init();
